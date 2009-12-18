f5be080d87d12afe71d1942bdbdcd234a971665a
Webserver User <u81451@oidipous.ispgateway.de>
2009-12-18 19:11:45 +0100

    Minor Improvements of Ajax

--- semanticweblog.js
+++ semanticweblog.js
@@ -13,8 +13,8 @@ function semanticweblog_send_assoc(){
   url += '/' + term1 + '/' + atid + '/' + term2;
   //alert (url);
   
-  if (document.getElementById) { // Nur Browser die "document.getElementById" koennen duerfen weitermachen
-     // Browserweiche: IE braucht ActiveX, alle anderen koennen es direkt (if else Geschichte)
+  if (document.getElementById) { // Only "document.getElementById" Browsers are allowed beyond here
+     // Browsergate: IE uses ActiveX, all other can go
      var http_request = (window.ActiveXObject) ? new ActiveXObject("Microsoft.XMLHTTP") : new XMLHttpRequest();
   }//if
 
@@ -22,16 +22,17 @@ function semanticweblog_send_assoc(){
   function() {
      if ((http_request.readyState == 4) && (http_request.status == 200)) {
         if(append) {
-           document.getElementById(target).innerHTML += "<br/>"+http_request.responseText; // Dranhängen
+           document.getElementById(target).innerHTML += "<br/>"+http_request.responseText; // Attach the Result
         }else{
-           document.getElementById(target).innerHTML = http_request.responseText; // Überschreiben
+           document.getElementById(target).innerHTML = http_request.responseText; // Or Replace the Result
         }
-        document.getElementById(target).scrollTop = document.getElementById(target).scrollHeight;
-       
-        //document.getElementById("loading").style.display = "none";
+        document.getElementById(target).scrollTop = document.getElementById(target).scrollHeight;       
      }// if
   }//function
   ;
   http_request.open(method,url,true); // Get the data, which file?, loading asynchronously is true
   http_request.send(null); // Es werden keine Daten transferiert, darum
+  // The two Term fields are being cleared
+  document.getElementById('edit-role1-1').value = '';
+  document.getElementById('edit-role2-1').value = '';
 }//function
